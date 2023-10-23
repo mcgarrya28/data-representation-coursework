df0406d5f936748744ba28dc23f196366d84afbe
Anthony <mcgarrya28@gmail.com>
2023-10-23 14:27:17 +0100

    Initial attempt CSO json file

---
+++ assignment03-cso.py
@@ -0,0 +1,9 @@
+# Importing the 2 libraries required , request and json
+
+import requests
+import json
+
+# Define the URL of the dataset to retrieve. 
+
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
+
